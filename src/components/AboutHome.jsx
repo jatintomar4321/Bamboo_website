eba7f8c6b86873128ad5af7460cac974afdead23
webwizzz <jatin.tomar@somaiya.edu>
2025-02-14 18:18:48 +0530

    about header

--- src/components/AboutHome.jsx
+++ src/components/AboutHome.jsx
@@ -13,7 +13,7 @@ function AboutHome() {
       initial="hidden"
       animate="visible"
     >
-      <div className="w-full  mx-auto px-4 sm:px-6 md:px-8 lg:px-6 py-2 sm:py-2 md:py-2 lg:py-2">
+      <div className="w-full  mx-auto px-4 sm:px-6 md:px-8 lg:px-6 py-20 sm:py-2 md:py-2 lg:py-2">
         <div className="flex flex-col gap-48 sm:gap-16 md:gap-24 lg:gap-36">
           <motion.h1 
             className="text-[clamp(7rem,12vw,11.5rem)] font-semibold leading-[0.85] tracking-tight"
